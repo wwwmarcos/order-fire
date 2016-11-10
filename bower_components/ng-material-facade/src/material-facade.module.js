8b9b338c7079c4c4f2c00fd87537560cc572494b
dstepbw <dstepbw@gmail.com>
2016-11-09 23:53:59 -0200

    up

---
+++ bower_components/ng-material-facade/src/material-facade.module.js
@@ -0,0 +1,10 @@
+(function() {
+  'use strict';
+   
+   angular
+    .module('MaterialFacade', [
+        'MaterialInputTextDirective'
+      , 'MaterialInputEmailDirective'
+      , 'ConfigMaterialFacade'
+    ])
+})();
